c292162b081aa2188a4ddcf5787b2d8b7a7c044a
GayDuck <alex.gaidukov@yandex.ru>
2025-02-28 19:29:57 +0300

    First "HelloWorld" program

---
+++ src/main/java/org/javaguru/student/lesson_1/HelloWorld.java
@@ -0,0 +1,9 @@
+package org.javaguru.student.lesson_1;
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, my name is Alex");
+    }
+}
