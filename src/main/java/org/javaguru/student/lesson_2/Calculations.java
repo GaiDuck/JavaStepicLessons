f08aa7f03dbc7f51430f687597627f589e29c591
GayDuck <alex.gaidukov@yandex.ru>
2025-03-01 20:27:46 +0300

    Add Calculations: Sub, Mult, Div

--- src/main/java/org/javaguru/student/lesson_2/Calculations.java
+++ src/main/java/org/javaguru/student/lesson_2/Calculations.java
@@ -7,12 +7,29 @@ public class Calculations
         var firstNumber = 10;
         var secondNumber = 15;
 
-        int sum = Sum(firstNumber, secondNumber);
-        System.out.println(firstNumber + "+" + secondNumber + "=" + sum);
+        System.out.println(Sum(firstNumber, secondNumber));
+        System.out.println(Sub(firstNumber, secondNumber));
+        System.out.println(Mult(firstNumber, secondNumber));
+        System.out.println(Div(firstNumber, secondNumber));
     }
 
     static int Sum(int firstNumber, int secondNumber)
     {
         return firstNumber + secondNumber;
+    }
+
+    static int Sub(int firstNumber, int secondNumber)
+    {
+        return firstNumber - secondNumber;
+    }
+
+    static int Mult(int firstNumber, int secondNumber)
+    {
+        return firstNumber * secondNumber;
+    }
+
+    static double Div(int firstNumber, int secondNumber)
+    {
+        return (double) firstNumber / secondNumber;
     }
 }
